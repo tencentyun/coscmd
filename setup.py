862ca477360b5884c2bad752de953d596e44ec2f
lewzylu <327874225@qq.com>
2017-07-10 15:29:34 +0800

    release

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def long_description():
         return fileobj.read()
 setup(
     name='coscmd',
-    version='1.4.2',
+    version='1.4.3',
     url='https://www.qcloud.com/',
     license='MIT',
     author='lewzylu',
