26b77a55b9db56a79844655ab6b4630f93016cce
lewzylu <327874225@qq.com>
2017-07-16 18:18:37 +0800

    修复之前最后一个bit没传上去的问题 (#13)
    * git pull
    
    * fix etag error
    
    * fix spare print

--- coscmd/cos_client.py
+++ coscmd/cos_client.py
@@ -146,7 +146,7 @@ class ObjectInterface(object):
                     if j+1 == self._retry:
                         return False
                 except Exception:
-                    logger.exception("upload file failed")
+                    logger.warn("upload file failed")
             return False
 
         def init_multiupload():
@@ -189,9 +189,6 @@ class ObjectInterface(object):
                         self._etag = 'ETag'
                     elif 'Etag' in rt.headers:
                         self._etag = 'Etag'
-                    else:
-                        logger.exception("ETag Error!")
-                        continue
                     self._md5[idx] = rt.headers[self._etag][1:-1]
                     if rt.status_code == 200:
                         self._have_finished += 1
@@ -201,7 +198,7 @@ class ObjectInterface(object):
                         time.sleep(2**j)
                         continue
                     if j+1 == retry:
-                        logger.exception("upload part failed: part{part}, round{round}, code: {code}".format(part=idx+1, round=j+1, code=rt.status_code))
+                        logger.warn("upload part failed: part{part}, round{round}, code: {code}".format(part=idx+1, round=j+1, code=rt.status_code))
                         return False
                 return True
 
@@ -211,7 +208,6 @@ class ObjectInterface(object):
             chunk_size = 1024 * 1024 * self._conf._part_size
             while file_size / chunk_size > 10000:
                 chunk_size = chunk_size * 10
-
             parts_num = file_size / chunk_size
             last_size = file_size - parts_num * chunk_size
             if last_size != 0:
@@ -228,7 +224,7 @@ class ObjectInterface(object):
             else:
                 for i in range(parts_num):
                     if i+1 == parts_num:
-                        pool.add_task(multiupload_parts_data, local_path, offset, file_size-offset-1, parts_num, i)
+                        pool.add_task(multiupload_parts_data, local_path, offset, file_size-offset, parts_num, i)
                     else:
                         pool.add_task(multiupload_parts_data, local_path, offset, chunk_size, parts_num, i)
                         offset += chunk_size
@@ -328,7 +324,7 @@ class ObjectInterface(object):
                 logger.warn(rt.content)
                 return False
         except Exception:
-            logger.exception("Error!")
+            logger.warn("Error!")
             return False
         return False
 
@@ -342,7 +338,7 @@ class ObjectInterface(object):
                  headers=rt.headers))
             return rt.status_code == 204
         except Exception:
-            logger.exception("Error!")
+            logger.warn("Error!")
             return False
         return False
 
@@ -372,7 +368,7 @@ class BucketInterface(object):
                  text=rt.text))
             return rt.status_code == 200
         except Exception:
-            logger.exception("Error!")
+            logger.warn("Error!")
             return False
         return True
 
@@ -388,7 +384,7 @@ class BucketInterface(object):
                  text=rt.text))
             return rt.status_code == 200
         except Exception:
-            logger.exception("Error!")
+            logger.warn("Error!")
             return False
         return True
 
