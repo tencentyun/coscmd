b4d110303a6ae02bd50cff8b1808362e90f620fd
liuchang0812 <liuchang0812@gmail.com>
2017-08-10 14:50:08 +0800

    update test
    Signed-off-by: liuchang0812 <liuchang0812@gmail.com>

--- coscmd/test.py
+++ coscmd/test.py
@@ -4,117 +4,100 @@ import logging
 import random
 import sys
 import os
-
 reload(sys)
-sys.setdefaultencoding('utf-8')
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.WARN, stream=sys.stdout, format="%(asctime)s - %(message)s")
-access_id = "AKID15IsskiBQKTZbAo6WhgcBqVls9SmuG00"
-access_key = "ciivKvnnrMvSvQpMAWuIz12pThGGlWRW"
+logging.basicConfig(level=logging.INFO, stream=sys.stdout, format="%(asctime)s - %(message)s")
+access_id = os.environ["ACCESS_ID"]
+access_key = os.environ["ACCESS_KEY"]
+test_num = 2
 file_id = str(random.randint(0, 1000)) + str(random.randint(0, 1000))
-file_list = []
-test_num = 20
-
-
-def gen_file(path, size):
-    file = open(path, 'w')
-    file.seek(1024*1024*size)
-    file.write('\x00')
-    file.close()
 
 
 def setUp():
-    print "config"
-    conf = cos_client.CosConfig(
-        appid="1252448703",
-        bucket="lewzylu05",
-        region="cn-north",
-        access_id=access_id,
-        access_key=access_key,
-        part_size=1,
-        max_thread=5
-    )
-    client = cos_client.CosS3Client(conf)
-    global obj_int
-    obj_int = client.obj_int()
-
-    conf = cos_client.CosConfig(
-                appid="1252448703",
-                bucket="buckettest",
-                region="cn-north",
-                access_id=access_id,
-                access_key=access_key,
-                part_size=1,
-                max_thread=5)
-    client = cos_client.CosS3Client(conf)
-    global buc_int
-    buc_int = client.buc_int()
+    print "Test interface"
 
 
 def tearDown():
-    print "function teardown"
+    print "test over"
 
 
-def Test_upload_object():
+def gen_file(path, size):
+    _file = open(path, 'w')
+    _file.seek(1024*1024*size)
+    _file.write('\x00')
+    _file.close()
+
+
+def Test():
     for i in range(test_num):
-        file_size = 3.1 * i + 0.1
+        bucket_id = str(random.randint(0, 1000)) + str(random.randint(0, 1000))
+        conf = cos_client.CosConfig(
+                    appid="1252448703",
+                    bucket="test" + str(bucket_id),
+                    region="cn-north",
+                    access_id=access_id,
+                    access_key=access_key,
+                    part_size=1,
+                    max_thread=5)
+        client = cos_client.CosS3Client(conf)
+        op_int = client.op_int()
+        print "Test create bucket " + conf._bucket
+        sys.stdout.flush()
+        rt = op_int.create_bucket()
+        assert rt
+        print "Test get bucket " + conf._bucket
+        sys.stdout.flush()
+        rt = op_int.get_bucket()
+        assert rt
+        print "Test put bucket acl " + conf._bucket
+        sys.stdout.flush()
+        rt = op_int.put_bucket_acl("anyone,43,123", None, "anyone")
+        assert rt
+        print "Test get bucket acl " + conf._bucket
+        sys.stdout.flush()
+        rt = op_int.get_bucket_acl()
+        assert rt
+        print "Test delete bucket " + conf._bucket
+        sys.stdout.flush()
+        rt = op_int.delete_bucket()
+        assert rt
+        conf = cos_client.CosConfig(
+            appid="1252448703",
+            bucket="lewzylu06",
+            region="cn-north",
+            access_id=access_id,
+            access_key=access_key,
+            part_size=1,
+            max_thread=5
+        )
+        client = cos_client.CosS3Client(conf)
+        op_int = client.op_int()
+        file_size = 5.1 * i + 0.1
         file_name = "tmp" + file_id + "_" + str(file_size) + "MB"
         print "Test upload " + file_name
         sys.stdout.flush()
         gen_file(file_name, file_size)
-        file_list.append([file_name, os.path.getsize(file_name)])
-        global obj_int
-        rt = obj_int.upload_file(file_name, file_name)
+        rt = op_int.upload_file(file_name, file_name)
         assert rt
-        os.remove(file_name)
-
-
-def Test_download_object():
-    for i in range(test_num):
-        file_name = file_list[i][0]
+        print "Test put object acl " + file_name
+        sys.stdout.flush()
+        rt = op_int.put_object_acl("anyone,43,123", None, "anyone", file_name)
+        assert rt
+        print "Test get object acl " + file_name
+        sys.stdout.flush()
+        rt = op_int.get_object_acl(file_name)
+        assert rt
         print "Test download " + file_name
         sys.stdout.flush()
-        global obj_int
-        rt = obj_int.download_file(file_name, file_name)
+        rt = op_int.download_file(file_name, file_name)
         assert rt
-        print "Test object size with " + file_name
-        assert os.path.getsize(file_name) == file_list[i][1]
         os.remove(file_name)
-
-
-def Test_delete_object():
-    for i in range(test_num):
-        file_name = file_list[i][0]
         print "Test delete " + file_name
         sys.stdout.flush()
-        global obj_int
-        rt = obj_int.delete_file(file_name)
+        rt = op_int.delete_file(file_name)
         assert rt
-
-
-def Test_create_bucket():
-    print "Test create bucket"
-    global buc_int
-    rt = buc_int.create_bucket()
-    assert rt
-
-
-def Test_get_bucket():
-    print "Test get bucket"
-    global buc_int
-    rt = buc_int.get_bucket()
-    assert rt
-
-
-def Test_delete_bucket():
-    print "Test delete bucket"
-    global buc_int
-    rt = buc_int.delete_bucket()
-    assert rt
 
 
 if __name__ == "__main__":
     setUp()
-    Test_upload_object()
-    Test_download_object()
-    Test_delete_object()
+    Test()
