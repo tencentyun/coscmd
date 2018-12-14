72e62404a4782c0c78a335b31990f622716a492d
lewzylu <327874225@qq.com>
2018-12-14 17:43:53 +0800

    update-copy

--- coscmd/cos_comm.py
+++ coscmd/cos_comm.py
@@ -2,6 +2,58 @@
 from six import text_type, binary_type
 from hashlib import md5
 
+maplist = {
+            'x-cos-copy-source-If-Modified-Since': 'CopySourceIfModifiedSince', 
+            'Content-Length': 'ContentLength',
+            'x-cos-server-side-encryption-cos-kms-key-id': 'SSEKMSKeyId',
+            'x-cos-server-side-encryption-customer-algorithm': 'SSECustomerAlgorithm',
+            'If-Unmodified-Since': 'IfUnmodifiedSince',
+            'response-content-language': 'ResponseContentLanguage',
+            'Metadata': 'Metadata',
+            'x-cos-grant-read': 'GrantRead',
+            'x-cos-copy-source-If-None-Match': 'CopySourceIfNoneMatch',
+            'Content-Language': 'ContentLanguage',
+            'x-cos-server-side-encryption': 'ServerSideEncryption',
+            'response-expires': 'ResponseExpires',
+            'Expires': 'Expires',
+            'Content-MD5': 'ContentMD5',
+            'response-content-disposition': 'ResponseContentDisposition',
+            'Referer': 'Referer',
+            'x-cos-grant-full-control': 'GrantFullControl',
+            'response-content-encoding': 'ResponseContentEncoding',
+            'Content-Disposition': 'ContentDisposition',
+            'If-Modified-Since': 'IfModifiedSince',
+            'versionId': 'VersionId',
+            'response-content-type': 'ResponseContentType',
+            'Range': 'Range',
+            'x-cos-server-side-encryption-customer-key-MD5': 'SSECustomerKeyMD5',
+            'x-cos-acl': 'ACL',
+            'x-cos-copy-source-If-Match': 'CopySourceIfMatch',
+            'Content-Encoding': 'ContentEncoding',
+            'x-cos-copy-source-If-Unmodified-Since': 'CopySourceIfUnmodifiedSince',
+            'response-cache-control': 'ResponseCacheControl',
+            'x-cos-server-side-encryption-customer-key': 'SSECustomerKey',
+            'x-cos-grant-write': 'GrantWrite',
+            'If-Match': 'IfMatch',
+            'x-cos-storage-class': 'StorageClass',
+            'Cache-Control': 'CacheControl',
+            'If-None-Match': 'IfNoneMatch',
+            'Content-Type': 'ContentType'
+        }
+
+def mapped(headers):
+    """coscmd到pythonsdk参数的一个映射"""
+    _headers = dict()
+    _meta = dict()
+    for i in headers:
+        if i in maplist:
+            _headers[maplist[i]] = headers[i]
+        elif i.startswith('x-cos-meta-'):
+            _meta[i] = headers[i]
+        else:
+            raise Exception('No Parameter Named ' + i + ' Please Check It')
+    _headers['Metadata'] = _meta
+    return _headers
 
 def to_bytes(s):
     """将字符串转为bytes"""
