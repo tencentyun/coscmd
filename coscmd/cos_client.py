07022ce0aa3f25e1fbe4b62759314e3e2084bba3
lewzylu <lewzylu@tencent.com>
2019-03-14 10:33:24 +0800

    py3 fixed

--- coscmd/cos_client.py
+++ coscmd/cos_client.py
@@ -17,7 +17,6 @@ import datetime
 import pytz
 import yaml
 import fnmatch
-from Queue import Queue
 from tqdm import tqdm
 from logging.handlers import RotatingFileHandler
 from wsgiref.handlers import format_date_time
