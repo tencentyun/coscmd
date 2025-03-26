aff074b66eaf9dbf977830b655660612a9c46f2c
lewzylu <327874225@qq.com>
2025-03-26 14:51:24 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     description='simple command for cos',
     long_description=long_description(),
     packages=find_packages(),
-    python_requires='>=3.6',
+    python_requires='>=2.7',
     install_requires=requirements(),
     entry_points={
         'console_scripts': [
